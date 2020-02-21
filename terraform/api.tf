5a53645ed0186ffe13e69c69c936adbd8a6ff6f8
Yan Zhou <38987117+yanzhoushow@users.noreply.github.com>
2020-02-21 15:00:38 -0500

    Update api.tf

--- terraform/api.tf
+++ terraform/api.tf
@@ -4,6 +4,43 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_lambda_function" "example" {
+  function_name = "helloworld-lambda"
+
+  # The bucket name as created earlier with "aws s3api create-bucket"
+  s3_bucket = "x-terraform-lambda"
+  s3_key    = "v1.0.0/helloworld.zip"
+
+  # "main": filename within the zip file (main.py)
+  # "lambda_handler": name of the property under which the handler function
+  handler = "main.lambda_handler"
+  runtime = "python3.8"
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+# IAM role which dictates what other AWS services the Lambda function
+# may access.
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_example_lambda"
+
+  assume_role_policy = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [
+     {
+       "Action": "sts:AssumeRole",
+       "Principal": {
+         "Service": "lambda.amazonaws.com"
+       },
+       "Effect": "Allow",
+       "Sid": ""
+     }
+   ]
+}
+ EOF
+}
+
 resource "aws_api_gateway_rest_api" "example" {
   name = "example"
   description = "example gateway"
